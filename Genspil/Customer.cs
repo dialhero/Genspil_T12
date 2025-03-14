31545d383395647b24bab50bfe4c9fc1289c769f
Megan <meganebsen@gmail.com>
2025-03-14 15:55:06 +0100

    AddCustomer

--- Genspil/Customer.cs
+++ Genspil/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Genspil
 {
@@ -13,6 +14,55 @@ namespace Genspil
         private string _name;
         private string _email;
         private int _phoneNumber;
+    
 
+    public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+    public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+    public int PhoneNumber
+        {
+            get => _phoneNumber;
+            set => _phoneNumber = value;
+        }
+
+        public Customer()
+        {
+            _name = "ukendt";
+            _email = "ukendt";
+            _phoneNumber = 00000000;
+        }
+
+
+        public Customer (string name, string email, int phoneNumber)
+        {
+            _name = name ?? "ukendt";
+            _email = email ?? "ukendt";
+            _phoneNumber = phoneNumber > 0 ? phoneNumber : 00000000;
+        }
+    
+        public static List<Customer> CustomerList = new List<Customer>();
+
+        public void AddCustomer(string name, string email, int phoneNumber)
+        {
+            Customer newCustomer = new Customer(name, email, phoneNumber);
+            CustomerList.Add(newCustomer);
+            Console.WriteLine($"Kunde {newCustomer.Name} tilføjet!");
+
+        }
+
+
+        public void DeleteCustomer(int phoneNumber)
+        { 
+        
+        }
     }
 }
--- Genspil/Program.cs
+++ Genspil/Program.cs
@@ -4,15 +4,18 @@
     {
         static void Main(string[] args)
         {
+            Customer customerManager = new Customer();  // Instans af Customer til at tilføje kunder
 
 
             while (true)
             {
                 Console.WriteLine("\nSelect an option:");
-                Console.WriteLine("1. tilføj et spil");
-                Console.WriteLine("2. print listen af spil");
-                Console.WriteLine("3. print filen ud");
-               
+                Console.WriteLine("1. Tilføj et spil");
+                Console.WriteLine("2. Print listen af spil");
+                Console.WriteLine("3. Print filen ud");
+                Console.WriteLine("4. Tilføj kunde");
+                Console.WriteLine("5. Fjern kunde");
+
 
                 string choice = Console.ReadLine();
 
@@ -24,9 +27,32 @@
                     case "2":
                         Boardgame.PrintListWares();
                         break;
-                        case "3":
+
+                    case "3":
                         Files.PrintFromFile("boardgames.txt");
                         break;
+
+                    case "4":
+                        Console.Write("\nIndtast kundens navn:");
+                        string name = Console.ReadLine();
+
+                        Console.Write("\nIndtast kundens email:");
+                        string email = Console.ReadLine();
+
+                        Console.Write("\nIndtast kundens telefonnummer:");
+                        int phoneNumber;
+                        while (!int.TryParse(Console.ReadLine(), out phoneNumber))
+                        {
+                            Console.WriteLine("Fejl! Indtast et gyldigt telefonnummer:");
+                        }
+
+                        customerManager.AddCustomer(name, email, phoneNumber);
+                        break;
+
+                    case "5":
+                        ;
+                        break;
+
                     default:
                         return;
 
