cc0b00ed6fd71bca2cbf6dcedc38d64861ef2c2b
Jeppe Lyngesen <jeppe_lyngesen@hotmail.com>
2025-03-19 14:35:44 +0100

    Constructor til Request

--- Genspil/Request.cs
+++ Genspil/Request.cs
@@ -9,6 +9,14 @@ namespace Genspil
     internal class Request
     {
         private int _amount;
-       
+        private Boardgame _boardGame;
+        private Customer _customer;
+
+        public Request(int amount, Boardgame boardGame, Customer customer)
+        {
+            _amount = amount;
+            _boardGame = boardGame;
+            _customer = customer;
+        }
     }
 }
