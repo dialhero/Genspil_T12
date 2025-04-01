e1d496f080b3a7c6a8181015a8a6250e92729bbe
daniel Rørlund <danielkargaard@hotmail.com>
2025-04-01 20:10:59 +0200

    Ændret igen for træning

--- Genspil/Boardgame.cs
+++ Genspil/Boardgame.cs
@@ -136,7 +136,7 @@ namespace Genspil
                 Console.WriteLine("Hov! Der var ikke noget, der passede på dine søgekriterier!");
             }
         }
-        public static void PrintListWares()
+        public static void PrintListWares22()
         {
             
             var sortedBoardgames = Program.allBoardgames.OrderBy(bg => bg.Name).ToList();
@@ -151,7 +151,23 @@ namespace Genspil
                 Console.WriteLine("| {0,-5} | {1,-30} | {2,-20} | {3,-8} | {4,-8} | {5,-9} | {6,-5} |", Program.allBoardgames.IndexOf(boardgame), boardgame.Name, boardgame.Edition, boardgame.PlayerAmount, boardgame.Price, boardgame.GameCondition, boardgame.Amount);
                 Console.WriteLine("+---------------------------------------------------------------------------------------------------------+");
                 
-                          }
+            }
+        }
+        public static void PrintListWares()
+        {
+            var sortedBoardgames = Program.allBoardgames.OrderBy(bg => bg.Name).ToList();
+
+            Console.Write($@"
+| {"Index",-5}   | {"Navn",-30} | {"Udgave",-20} | {"Spillere",-8} | {"Pris",-8:f2} | {"Stand",-9} | {"Antal",-5} |
+|-----------------------------------------------------------------------------------------------------------|");
+            foreach (var boardgame in sortedBoardgames)
+            {
+                int x = Program.allBoardgames.IndexOf(boardgame);
+                Console.Write($@"
+| {Program.allBoardgames.IndexOf(boardgame),-5}   | {boardgame.Name,-30} | {boardgame.Edition,-20} | {boardgame.PlayerAmount,-8} | {boardgame.Price,-8:f2} | {boardgame.GameCondition,-9} | {boardgame.Amount,-5} |
+|-----------------------------------------------------------------------------------------------------------|");
+
+            }
         }
 
     }
