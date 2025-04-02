0e03e7734157e845acbbe7ce61c0224011dbcea9
daniel Rørlund <danielkargaard@hotmail.com>
2025-04-02 13:25:31 +0200

    .

--- Genspil/Boardgame.cs
+++ Genspil/Boardgame.cs
@@ -85,7 +85,7 @@ namespace Genspil
         }
 
 
-        public static void PrintListWaress()
+        public static void PrintListWares()
         {
             var sortedBoardgames = Program.allBoardgames.OrderBy(bg => bg.Name).ToList();
 
@@ -136,39 +136,8 @@ namespace Genspil
                 Console.WriteLine("Hov! Der var ikke noget, der passede på dine søgekriterier!");
             }
         }
-        public static void PrintListWares22()
-        {
-            
-            var sortedBoardgames = Program.allBoardgames.OrderBy(bg => bg.Name).ToList();
-
-
-            Console.WriteLine("+---------------------------------------------------------------------------------------------------------+");
-            Console.WriteLine("| {0,-5} | {1,-30} | {2,-20} | {3,-8} | {4,-8} | {5,-9} | {6,-5} |", "Index", "Navn", "Edition", "Players", "Pris", "Stand", "Antal");
-            Console.WriteLine("+---------------------------------------------------------------------------------------------------------+");
-            foreach (var boardgame in sortedBoardgames)
-            {
-                int x = Program.allBoardgames.IndexOf(boardgame);
-                Console.WriteLine("| {0,-5} | {1,-30} | {2,-20} | {3,-8} | {4,-8} | {5,-9} | {6,-5} |", Program.allBoardgames.IndexOf(boardgame), boardgame.Name, boardgame.Edition, boardgame.PlayerAmount, boardgame.Price, boardgame.GameCondition, boardgame.Amount);
-                Console.WriteLine("+---------------------------------------------------------------------------------------------------------+");
-                
-            }
-        }
-        public static void PrintListWares()
-        {
-            var sortedBoardgames = Program.allBoardgames.OrderBy(bg => bg.Name).ToList();
-
-            Console.Write($@"
-| {"Index",-5}   | {"Navn",-30} | {"Udgave",-20} | {"Spillere",-8} | {"Pris",-8:f2} | {"Stand",-9} | {"Antal",-5} |
-|-----------------------------------------------------------------------------------------------------------|");
-            foreach (var boardgame in sortedBoardgames)
-            {
-                int x = Program.allBoardgames.IndexOf(boardgame);
-                Console.Write($@"
-| {Program.allBoardgames.IndexOf(boardgame),-5}   | {boardgame.Name,-30} | {boardgame.Edition,-20} | {boardgame.PlayerAmount,-8} | {boardgame.Price,-8:f2} | {boardgame.GameCondition,-9} | {boardgame.Amount,-5} |
-|-----------------------------------------------------------------------------------------------------------|");
-
-            }
-        }
+       
+        
 
     }
 }
